7e50276df228d1dbc2f4e7de45e75d9cedc187ee
zhengweibin <3254560365@qq.com>
2026-04-13 00:40:27 +0800

    加了自动化部署、连云后端

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://8.138.10.215:8081',
+        // target: 'http://localhost:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       }
